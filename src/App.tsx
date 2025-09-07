8352bbea05a0c28aaea6da9435de8deaec814acb
saraivaa.me <wesley-saraiva@outlook.com>
2025-09-06 21:13:37 -0300

    inciando projeto

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-gray-200 flex items-center justify-center p-4">
+      <p>GAME BOY</p>
+    </div>
+  );
+}
+
+export default App;
